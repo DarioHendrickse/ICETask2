31623fd1debae4fcfc5c16d97ca82a7459ff767a
DarioHendrickse <st10436137@vcconnect.edu.za>
2025-03-14 08:53:47 +0200

    Commit 2
    Cleaned up some code and added comments. Created new folder with .wav files as the bin/debug is not included in the git repo.

--- WAVPlayer.cs
+++ WAVPlayer.cs
@@ -10,27 +10,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Media;
+using System.IO;
 
 namespace MediaPlayerWAV
 {
     class WAVPlayer
     {
-        /*
-        private SoundPlayer WAV1 = new SoundPlayer(@"gump.wav");
-        private SoundPlayer WAV2 = new SoundPlayer(@"buttocks.wav");
-        private SoundPlayer WAV3 = new SoundPlayer(@"box_of_chocolates_x.wav");
-        */
+        //initialising while loop flag to end program
         private bool endProgram = false;
-        private string welcome = "Welcome to the WAV Sound Player!\n(Inspired by Forrest Gump)\nWhich Sound would you like to hear?";
-        private string wav1 = "1. Hello";
-        private string wav2 = "2. Where is the pain?";
-        private string wav3 = "3. Wise Words";
 
+        //initialising console output as constants
+        const string welcome = "Welcome to the WAV Sound Player!\n(Inspired by Forrest Gump)\nWhich Sound would you like to hear?";
+        const string wav1 = "1. Hello";
+        const string wav2 = "2. Where is the pain?";
+        const string wav3 = "3. Wise Words";
+
+        //method to execute the console application
         public void Run()
         {
             Console.WriteLine($"{welcome}\n{wav1}\n{wav2}\n{wav3}");
 
-            while (endProgram==false)
+            //creating while loop to run the application so long as the user wishes to not end program
+            //based on user input, the correct sound will be played
+            while (endProgram == false)
             {
 
                 var input = Console.ReadLine();
@@ -38,58 +40,73 @@ namespace MediaPlayerWAV
                 switch (input.Trim())
                 {
                     case "1":
-                        PlaySound("gump.wav");
-
-                        Console.Clear();
-                        Console.WriteLine($"{welcome}");
-
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine(wav1);
-
-                        Console.ResetColor();
-                        Console.WriteLine($"{wav2}\n{wav3}");
-
+                        PlaySound("gump.wav", 1);
                         break;
                     case "2":
-                        PlaySound("buttocks.wav");
-
-                        Console.Clear();
-                        Console.WriteLine($"{welcome}\n{wav1}");
-
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine(wav2);
-
-                        Console.ResetColor();
-                        Console.WriteLine(wav3);
-
+                        PlaySound("buttocks.wav", 2);
                         break;
                     case "3":
-                        PlaySound("box_of_chocolates_x.wav");
-                        
-                        Console.Clear();
-                        Console.WriteLine($"{welcome}\n{wav1}\n{wav2}");
-
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine(wav3);
-
-                        Console.ResetColor();
-
+                        PlaySound("box_of_chocolates_x.wav", 3);
                         break;
                     default:
                         endProgram = true;
                         break;
                 }
             }
-            
+
         }
 
-        public void PlaySound(String file)
+        /// <summary>
+        /// Takes the .wav sound file name and an integer to determine which choice was made
+        /// Plays the sound and prints same welcome message and choices, but the .wav that was just played is now green
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="wav"></param>
+        public void PlaySound(String file, int wav)
         {
+
+            Console.WriteLine("\nPlaying sound...");
+
             SoundPlayer sound = new SoundPlayer();
             sound.SoundLocation = file;
             sound.PlaySync();
 
+            switch (wav)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine($"{welcome}");
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(wav1);
+
+                    Console.ResetColor();
+                    Console.WriteLine($"{wav2}\n{wav3}");
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine($"{welcome}\n{wav1}");
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(wav2);
+
+                    Console.ResetColor();
+                    Console.WriteLine(wav3);
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine($"{welcome}\n{wav1}\n{wav2}");
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(wav3);
+
+                    Console.ResetColor();
+                    break;
+                default:
+                    break;
+            }
         }
+
 
     }
 }
